cb8742b5c357b227c2b0114e11f0bc8dbd33382e
Raj Chintawar <chintawarraj@gmail.com>
2026-01-29 08:30:59 +0530

    Traverse,insertion, deletion in array

--- aim2.java
+++ aim2.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
 class Aim2 {
     public static void main(String[] args) {
-        int arr[] = {10, 20, 30, 40, 50};
-        int n = 5;
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add (10);
+        arr.add (20);
+        arr.add (30);
+        arr.add (40);
+        arr.add (50);
+        int n = arr.size();
         System.out.println("Array elements are: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i]+" ");
+            System.out.println(arr+" ");
+
+
+            arr.add(5,60);
+            System.out.println("Inserted new element: ");
+            System.out.println(arr+" " );
+
+
+
+            arr.remove(5);
+            System.out.println("After removing element: ");
+        System.out.println(arr+" " );
         }
     }
-}
