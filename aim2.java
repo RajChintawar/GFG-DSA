1e647d56e2ed446899c9981d09cdcd56a2ef074f
Raj Chintawar <chintawarraj@gmail.com>
2026-01-29 08:19:04 +0530

    Aim 2 traversing

---
+++ aim2.java
@@ -0,0 +1,10 @@
+class Aim2 {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        int n = 5;
+        System.out.println("Array elements are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
